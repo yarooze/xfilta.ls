ad34e3e77ea3e9cfec1f3713a0a8606341199685
Jaroslav Bleicher <j.bleicher@circit.de>
2019-05-06 07:49:20 +0200

    icon

--- xfilta.user.js
+++ xfilta.user.js
@@ -13,48 +13,84 @@
 // ==/UserScript==
 (function() {
     'use strict';
-var doc = document,rgtClickContextMenu;
-    doc.oncontextmenu = function(e) {
-    var element = e.target;
-        if (element.tagName.toLocaleLowerCase() !== "select") {
-            return;
-        }
-        e.preventDefault();
-        var filterString = window.prompt("xFilta", "");
-        if (filterString) {
-            filterSelectContent(filterString, element);
-        } else {
-            resetFilteredContent(element);
-        }
-};
-    function resetFilteredContent(select) {
-        var option;
-        for (var optionIdx = 0; optionIdx < select.length; optionIdx++) {
-            option = select[optionIdx];
-            option.style.display = ""; //"none"
-            option.disabled = false;
-        }
-    };
-    function filterSelectContent(f_word, select) {
-        var option, optionValue;
-        f_word = f_word.toLowerCase();
-        console.log("filterSelectContent", f_word, select);
-        if (!f_word) {
-            self.resetFilteredContent(select);
-        } else {
+
+    var XFILTA = {
+        findFrames: function (el, cb) {
+            setTimeout(function () {
+                if (!el) {
+                    console.log('No frames. Stop.');
+                    return;
+                }
+
+                el.oncontextmenu = function(e) {
+                    var element = e.target;
+                    if (element.tagName.toLocaleLowerCase() !== "select") {
+                        return;
+                    }
+                    e.preventDefault();
+                    var filterString = window.prompt("xFilta", "");
+                    if (filterString) {
+                        XFILTA.filterSelectContent(filterString, element);
+                    } else {
+                        XFILTA.resetFilteredContent(element);
+                    }
+                };
+
+                var iframes = el.getElementsByTagName('iframe');
+                if (iframes && iframes.length) {
+                    var iframeDocument = iframes[0].contentDocument || iframes[0].contentWindow.document;
+                    if (typeof cb == 'function') {
+                        cb(iframeDocument, XFILTA.findFrames);
+                    }
+                }
+
+                var frames = el.getElementsByTagName("frame");
+                if (frames && frames.length) {
+                    var frameDocument = frames[0].contentDocument || frames[0].contentWindow.document;
+                    if (typeof cb == 'function') {
+                        cb(frameDocument, XFILTA.findFrames);
+                    }
+                }
+
+            }, 100);
+        },
+        resetFilteredContent: function (select) {
+            var option;
             for (var optionIdx = 0; optionIdx < select.length; optionIdx++) {
                 option = select[optionIdx];
-                optionValue = option.textContent;
-                optionValue = optionValue.toLowerCase();
+                option.style.display = ""; //"none"
+                option.disabled = false;
+            }
+        },
+        filterSelectContent: function (f_word, select) {
+            var option, optionValue;
+            f_word = f_word.toLowerCase();
+            if (!f_word) {
+                XFILTA.resetFilteredContent(select);
+            } else {
+                for (var optionIdx = 0; optionIdx < select.length; optionIdx++) {
+                    option = select[optionIdx];
+                    optionValue = option.textContent;
+                    optionValue = optionValue.toLowerCase();
 
-                if (optionValue.indexOf(f_word) === -1) {
-                    option.style.display = "none";
-                    option.disabled = "disabled";
-                } else {
-                    option.style.display = "";
-                    option.disabled = false;
+                    if (optionValue.indexOf(f_word) === -1) {
+                        option.style.display = "none";
+                        option.disabled = "disabled";
+                    } else {
+                        option.style.display = "";
+                        option.disabled = false;
+                    }
                 }
             }
+        },
+        start: function () {
+            setInterval(function() {
+                console.log("XFILTA searching for frames");
+                XFILTA.findFrames(document, XFILTA.findFrames);
+            }, 10000);
+            console.log("XFILTA start");
         }
     };
+
+    XFILTA.start();
 })();
